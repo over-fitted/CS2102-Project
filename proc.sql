d2962a536ff0879f3618aaa9d7f85f7e58033d1b
Aryan Sarswat <aryansarswat2000@gmail.com>
2021-10-18 00:06:16 +0800

    Update proc.sql

--- proc.sql
+++ proc.sql
@@ -15,7 +15,7 @@ BEGIN
     FROM Contact c
     WHERE NEW.eid = c.eid;
     IF (number = 0) THEN 
-        RAISE NOTICE 'Employee has no contact, insert blocked';
+        RAISE EXCEPTION 'Employee has no contact, insert blocked';
         RETURN NULL;
     ELSE
         RETURN NEW;
@@ -100,8 +100,125 @@ CREATE OR REPLACE TRIGGER _t_bookingWithinCapacity
 BEFORE INSERT ON Participates
 FOR EACH ROW EXECUTE FUNCTION _tf_bookingWithinCapacity();
 
+-- ### A Booking which is not immedietely approved by manager is removed
+CREATE OR REPLACE FUNCTION _tf_deleteBooking() 
+RETURNS TRIGGER AS $$
+BEGIN
+    -- ### Checking if manager has approved
+    IF ((SELECT b.approver.id FROM Bookings b WHERE b.booker_id = NEW.booker_id) IS NULL) THEN
+        RAISE EXCEPTION 'Booking is not approved by manager, Booking deleted';
+        RETURN NULL;
+    ELSE
+        RETURN NEW;
+    END IF;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE CONSTRAINT TRIGGER _t_bookingNotApproved
+AFTER INSERT ON Bookings
+DEFERRABLE INITIALLY DEFERRED
+FOR EACH ROW EXECUTE FUNCTION _tf_deleteBooking();
+
 /* FUNCTIONS */
 
+/*
+CREATE OR REPLACE FUNCTION _f_contact_tracing
+    (IN employeeId)
+RETURNS TABLE(closeContactEmployeeId INTEGER) AS $$
+DECLARE
+    curs CURSOR FOR (
+        SELECT p.room, p.floor, p.date, p.time 
+        FROM Participates p 
+        WHERE p.eid);
+    r RECORD;
+BEGIN
+    OPEN curs;
+    CREATE TEMP TABLE closeContactId (empId INTEGER);
 
+    LOOP
+        FETCH curs INTO r;
+        EXIT WHEN NOT FOUND;
+        closeContactId UNION (
+            SELECT p.eid 
+            FROM Participates p 
+            WHERE r.room = p.room
+                AND r.floor = p.floor
+                AND r.date = p.date
+                AND r.time = p.time)
+    END LOOP
+    CLOSE curs;
+END;
+$$ LANGUAGE plpgsql
+*/
 
 /* PROCEDURES */
+
+CREATE OR REPLACE PROCEDURE _p_approve_meeting
+    (IN floorNumber INTEGER, IN roomNumber INTEGER, IN inputDate DATE, IN startHour TIMESTAMP, IN endHour TIMESTAMP, IN managerEid INTEGER)
+AS $$
+
+<<BeginLabel>>
+DECLARE
+    tempStartHour INTEGER := startHour;
+    employeeId INTEGER;
+    employeeDept INTEGER;
+    managerDept INTEGER;
+BEGIN   
+    -- ### Ensure input is correct
+    IF (startHour > endHour) THEN
+        EXIT BeginLabel;
+    END IF;
+    
+    <<MainLoop>>
+    LOOP
+        -- ### All the bookings have been approved then exit
+        EXIT WHEN tempStartHour > endHour;
+        
+        -- ### Checks whether employee's department is the same as the manager's department
+        SELECT e.did INTO managerDept
+        FROM Employees e
+        WHERE e.eid = managerEid;
+
+        SELECT b.booker_id INTO employeeId
+        FROM Bookings b
+        WHERE b.floor = floorNumber
+                AND b.room = roomNumber
+                AND b.date = inputDate
+                AND startHour = startHour;
+        
+        SELECT b.did INTO employeeDept
+        FROM Employees e
+        WHERE e.eid = employeeId;
+
+        -- ### Approve all bookings until employeeDept != mangerDept
+        IF (managerDept = employeeDeptId) THEN
+            UPDATE Bookings b
+                SET approver_id = managerEid
+                WHERE 
+                    b.floor = floorNumber
+                    AND b.room = roomNumber
+                    AND b.date = inputDate
+                    AND startHour = tempStartHour;
+            
+            tempStartHour = tempStartHour + 1;
+        ELSE
+            RAISE EXCEPTION 'Employee Department and Manger Department are different';
+            EXIT MainLoop;
+        END IF;
+    END LOOP;
+END;
+
+$$ LANGUAGE plpgsql;
+
+CREATE OR REPLACE PROCEDURE _p_declare_health
+    (IN employeeId INTEGER, IN inDate DATE, IN temperature INTEGER)
+AS $$
+BEGIN
+    INSERT INTO HealthDeclaration
+        VALUES (
+        inDate,
+        employeeId,
+        temperature
+    ); 
+END;
+$$ LANGUAGE plpgsql;
