074e56e8ffb27ad389e75cb35971d4eac9c1277f
Andrew Sutjipto <overfitted@gmail.com>
2021-11-08 03:31:09 +0800

    add fever event tests

--- positive_test.sql
+++ positive_test.sql
@@ -200,10 +200,125 @@ Test Case:
 
 --
 -- FEVER
---
+-- VERSION 2
+-- Booker Declaring Fever & Close Contact
 
--- 8) Show non-compliance before declarations
+/*
+Bookings Table
+ room | floor |    date    |   time   | booker_id | approver_id
+------+-------+------------+----------+-----------+-------------
+    1 |     1 | 2021-01-01 | 00:00:00 |         1 |           1
+    3 |     3 | 2021-01-01 | 05:00:00 |         4 |           1
+    4 |     4 | 2021-01-01 | 03:00:00 |         8 |           1
+    5 |     5 | 2021-01-04 | 00:00:00 |         7 |           1
+    6 |     6 | 2021-01-02 | 00:00:00 |         7 |           1
+    8 |     8 | 2021-01-04 | 03:00:00 |         7 |
+(6 rows)
 
--- 9) Show normal compliance no fever + non-compliance function
+Participates
+ eid | room | floor |    date    |   time
+-----+------+-------+------------+----------
+   1 |    1 |     1 | 2021-01-01 | 00:00:00
+   3 |    1 |     1 | 2021-01-01 | 00:00:00
+   4 |    1 |     1 | 2021-01-01 | 00:00:00
+   5 |    1 |     1 | 2021-01-01 | 00:00:00
+   6 |    1 |     1 | 2021-01-01 | 00:00:00
+   4 |    3 |     3 | 2021-01-01 | 05:00:00
+   8 |    4 |     4 | 2021-01-01 | 03:00:00
+   5 |    4 |     4 | 2021-01-01 | 03:00:00
+   7 |    5 |     5 | 2021-01-04 | 00:00:00
+   7 |    6 |     6 | 2021-01-02 | 00:00:00
+   8 |    6 |     6 | 2021-01-02 | 00:00:00
+   9 |    6 |     6 | 2021-01-02 | 00:00:00
+   7 |    8 |     8 | 2021-01-04 | 03:00:00
+(13 rows)
+*/
 
--- 10) Show fever event on january 1
+
+BEGIN;
+-- Add Departments
+CALL add_department (0, 'Human Resources');
+
+-- Create Rooms (Fever_Room will contain personel with fever)
+CALL add_room(1, 1, 'FEVER_ROOM', 0, 13);
+CALL add_room(2, 2, 'Room_2', 0, 14);
+CALL add_room(3, 3, 'Room_3', 0, 5);
+CALL add_room(4, 4, 'Room_4', 0, 9);
+CALL add_room(5, 5, 'Room_5', 0, 16);
+CALL add_room(6, 6, 'Room_6', 0, 6);
+CALL add_room(7, 7, 'Room_7', 0, 6);
+CALL add_room(8, 8, 'Room_8', 0, 10);
+
+-- Create Employees (Will contain employees which will declare temperature above or below 37.5)
+
+--Fever employees
+CALL add_employee('Fever_1', NULL, NULL, '84399505', 'Manager', 0); -- Employee Id 1
+CALL add_employee('Fever_2', NULL, NULL, '39630089', 'Senior', 0); -- Employee Id 2
+
+--Close contact employees
+CALL add_employee('Close_contact_1', NULL, NULL, '51244095', 'Senior', 0); -- Employee Id 3
+CALL add_employee('Close_contact_2', NULL, NULL, '87467446', 'Senior', 0); -- Employee Id 4
+CALL add_employee('Close_contact_3', NULL, NULL, '12314552', 'Senior', 0); -- Employee Id 5
+CALL add_employee('Close_contact_4', NULL, NULL, '83413546', 'Senior', 0); -- Employee Id 6
+
+CALL add_employee('Non_Fever_1', NULL, NULL, '47534732', 'Senior', 0); -- Employee Id 7
+CALL add_employee('Non_Fever_2', NULL, NULL, '47534732', 'Senior', 0); -- Employee Id 8
+CALL add_employee('Non_Fever_3', NULL, NULL, '47534732', 'Senior', 0); -- Employee Id 9
+
+-- Rooms being booked
+
+--Booker will declare fever and is a past booking, booking will stay
+CALL book_room(1, 1,'2021-01-01', '00:00:00', '01:00:00', 1);
+CALL join_meeting(1, 1,'2021-01-01', '00:00:00', '01:00:00', 3);
+CALL join_meeting(1, 1,'2021-01-01', '00:00:00', '01:00:00', 4);
+CALL join_meeting(1, 1,'2021-01-01', '00:00:00', '01:00:00', 5);
+CALL join_meeting(1, 1,'2021-01-01', '00:00:00', '01:00:00', 6);
+
+--Booker has future meeting should be deleted
+CALL book_room(7, 7,'2021-01-03', '10:00:00', '11:00:00', 1);
+
+-- Employee 3 has a booking of his own in the future should be deleted
+CALL book_room(2, 2,'2021-01-03', '00:00:00', '02:00:00', 3);
+
+-- Employee 4 in booked a past meeting, no change
+CALL book_room(3, 3,'2021-01-01', '05:00:00', '06:00:00', 4);
+
+-- Employee 5 is a particpant in a past meeting, no change
+CALL book_room(4, 4,'2021-01-01', '03:00:00', '04:00:00', 8);
+CALL join_meeting(4, 4,'2021-01-01', '03:00:00', '04:00:00', 5);
+
+--Employee 6 is a participant in a future meeting, but is not the booker : removed from Booking
+CALL book_room(5, 5,'2021-01-04', '00:00:00', '01:00:00', 7);
+CALL join_meeting(5, 5,'2021-01-04', '00:00:00', '01:00:00', 6);
+
+--Booking should not change
+CALL book_room(6, 6,'2021-01-02', '00:00:00', '01:00:00', 7);
+CALL join_meeting(6, 6,'2021-01-02', '00:00:00', '01:00:00', 8);
+CALL join_meeting(6, 6,'2021-01-02', '00:00:00', '01:00:00', 9);
+
+CALL approve_meeting(1, 1,'2021-01-01', '00:00:00', '01:00:00', 1);
+CALL approve_meeting(2, 2,'2021-01-03', '00:00:00', '02:00:00', 1);
+CALL approve_meeting(3, 3,'2021-01-01', '05:00:00', '06:00:00', 1);
+CALL approve_meeting(4, 4,'2021-01-01', '03:00:00', '04:00:00', 1);
+CALL approve_meeting(5, 5,'2021-01-04', '00:00:00', '01:00:00', 1);
+CALL approve_meeting(6, 6,'2021-01-02', '00:00:00', '01:00:00', 1);
+CALL approve_meeting(7, 7,'2021-01-03', '10:00:00', '11:00:00', 1);
+
+SELECT * FROM Bookings;
+SELECT * FROM participates;
+
+--Employee 1 declare after 1 day
+CALL declare_health(1, '2021-01-02', 37.9, '02:00:00');
+
+-- Employee with fever trying to Making a booking
+CALL book_room(8, 8,'2021-01-01', '00:00:00', '01:00:00', 1);
+
+-- Employee with fever tries to participate in meeting
+CALL book_room(8, 8,'2021-01-04', '03:00:00', '04:00:00', 7);
+CALL join_meeting(8, 8,'2021-01-04', '03:00:00', '04:00:00', 1);
+
+SELECT * FROM Bookings;
+
+SELECT * FROM Participates;
+
+ROLLBACK;
