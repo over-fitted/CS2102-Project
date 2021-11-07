ea4d66922e76d768440818b10d0d5491554aa542
Andrew Sutjipto <overfitted@gmail.com>
2021-11-08 01:57:04 +0800

    fix positive test, add till resignation

--- positive_test.sql
+++ positive_test.sql
@@ -1,15 +1,13 @@
 \i schema.sql
 \i proc.sql
 
--- 5) show manager report for each manager starting from LATE date
--- 6) each manager approves 1 booking
--- 7) junior joins senior's booking
 -- 8) everyone joins 1 booking to show capacity
 -- 8) manager 2 has a fever
 -- 9) manager 1 leaves    
     
 -- SETUP
 -- 1) Add 2 departments
+BEGIN;
 CALL add_department(1, 'firstDep');
 CALL add_department(2, 'secondDep');
 
@@ -44,19 +42,34 @@ CALL book_room(1, 1, '2021-2-2'::DATE, '11:00:00'::TIME, '12:00:00'::TIME, 6);
 
 SELECT * FROM Bookings;
 
+-- Post-booking available rooms
+SELECT * FROM search_room(10, '2021-1-1', '10:00:00', '13:00:00');
+
 -- 5) Check manager report for both managers. This shows that book_room and manager report behaviour are consistent with expectations
+/*
+a) each manager can only view the bookings of employees from their own department
+b) start-date inclusive
+*/
 SELECT * FROM view_manager_report('2021-1-1', 1);
 SELECT * FROM view_manager_report('2021-2-1', 1);
 SELECT * FROM view_manager_report('2021-1-1', 5);
 
--- 6) 
-CALL join_meeting(1, 1, '2021-2-1'::DATE, '10:00:00'::TIME, '11:00:00'::TIME, 2);
-CALL join_meeting(1, 1, '2021-2-1'::DATE, '11:00:00'::TIME, '12:00:00'::TIME, 2);
-CALL join_meeting(1, 1, '2021-2-2'::DATE, '10:00:00'::TIME, '11:00:00'::TIME, 2);
-CALL join_meeting(1, 1, '2021-2-2'::DATE, '11:00:00'::TIME, '12:00:00'::TIME, 2);
+-- 6) everyone joins 1 booking to show capacity check
+CALL join_meeting(1, 1, '2021-1-1'::DATE, '10:00:00'::TIME, '11:00:00'::TIME, 2);
+CALL join_meeting(1, 1, '2021-1-1'::DATE, '10:00:00'::TIME, '11:00:00'::TIME, 3);
+CALL join_meeting(1, 1, '2021-1-1'::DATE, '10:00:00'::TIME, '11:00:00'::TIME, 4);
 
+-- 7) Have manager from department 1 approve bookings for own and other departments
 CALL approve_meeting(1, 1, '2021-1-1'::DATE, '10:00:00'::TIME, '11:00:00'::TIME, 1);
-CALL approve_meeting(1, 1, '2021-2-2'::DATE, '10:00:00'::TIME, '11:00:00'::TIME, 1);
+CALL approve_meeting(1, 1, '2021-2-2'::DATE, '11:00:00'::TIME, '12:00:00'::TIME, 1);
 
--- PRE-BOOKING CALLS
+SELECT * FROM view_manager_report('2021-1-1', 1);
+SELECT * FROM view_manager_report('2021-1-1', 5);
+
+-- Post-approval available rooms
 SELECT * FROM search_room(10, '2021-1-1', '10:00:00', '13:00:00');
+
+-- RESIGNATION
+
+
+ROLLBACK;
